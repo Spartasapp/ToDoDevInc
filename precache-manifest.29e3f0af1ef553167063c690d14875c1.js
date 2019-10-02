0db43cf7947d73f7e19a3f0a042cc2fe44bc931d
Siarhei <spartateambuilder@mail.ru>
2019-10-02 09:44:54 +0300

    Updates

---
+++ precache-manifest.29e3f0af1ef553167063c690d14875c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0201661ade9157eac41860c1a8ce6c3e",
+    "url": "/ToDoDevInc/index.html"
+  },
+  {
+    "revision": "b9e6b6360aa5c91cd7a6",
+    "url": "/ToDoDevInc/static/css/main.8a323353.chunk.css"
+  },
+  {
+    "revision": "be0ae8831abdbf7f9d54",
+    "url": "/ToDoDevInc/static/js/2.eaf776c0.chunk.js"
+  },
+  {
+    "revision": "b9e6b6360aa5c91cd7a6",
+    "url": "/ToDoDevInc/static/js/main.c060088f.chunk.js"
+  },
+  {
+    "revision": "3277a3e72c3ee8170e2f",
+    "url": "/ToDoDevInc/static/js/runtime-main.d4ef6159.js"
+  }
+]);
